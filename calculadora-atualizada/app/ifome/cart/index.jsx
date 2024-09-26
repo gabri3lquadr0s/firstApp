af68654d70931b7f43591c0b94ceb52438fce47a
gabri3lquadr0s <gabrielteodoro0710@gmail.com>
2024-09-25 21:14:39 -0300

    ifome finalização

--- calculadora-atualizada/app/ifome/cart/index.jsx
+++ calculadora-atualizada/app/ifome/cart/index.jsx
@@ -4,10 +4,123 @@ import { AppContext } from "../../../scripts/AppContext";
 import {View, Text, Image, StyleSheet, Button, Pressable, SafeAreaView, FlatList} from 'react-native';
 import Header from "../../../components/header";
 
-const App = () => {
-    return(
-        <SafeAreaView>
 
+const Item = ({id, name, vendor, price}) => {
+    return(
+        <View>
+            <View >
+                <Text style={styles.info}>{name}</Text>
+                <Text style={styles.info}>{vendor}</Text>
+            </View>
+            <View>
+                <Text style={styles.price}>R$ {price}</Text>
+            </View>
+        </View>
+    )
+}
+
+const App = () => {
+    const {cart, setCart} = useContext(AppContext);
+    const keyExtractor = (item, index) => index.toString();
+    const [total, setTotal] = useState(0);
+
+    const handleExcludeItem = (index) => {
+        const newCart = [...cart];
+        newCart.splice(index, 1);
+        setCart(newCart);
+    }
+
+    useEffect(() => {
+        let a = 0;
+        let b = cart.map((item) => {a = a + item.price});
+        setTotal(a.toFixed(2));
+    })
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Header link=".." title="Seu carrinho" />
+            </View>
+            <FlatList data={cart} renderItem={({item, index}) => (
+                    <View style={styles.item}>
+                        <Item name={item.name} vendor={item.vendor} price={item.price} id={item.id} />
+                        <Pressable onPress={() => handleExcludeItem(index)} style={styles.x}>
+                            <Text style={styles.xtxt}>X</Text>
+                        </Pressable>
+                    </View>
+                )} keyExtractor={keyExtractor}
+            />
+            <View>
+                <Text style={styles.total}>Total: R$ {total}</Text>
+                <View style={styles.end}>
+                    <Pressable onPress={() => setCart([])} style={styles.press}>
+                        <Text style={styles.pressTxt}>Limpar carrinho</Text>
+                    </Pressable>
+                    <Pressable style={styles.press}>
+                        <Text style={styles.pressTxt}>Finalizar compra</Text>
+                    </Pressable>
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+      marginBottom: 20,
+    },
+    container: {
+        flex: 1,
+    },
+    end: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+        padding: 15,
+    },
+    press: {
+        backgroundColor: '#007bff',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        alignItems: 'center',
+        width: 170,
+    },
+    pressTxt: {
+        color: 'white',
+    },
+    item: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 40,
+        borderWidth: 3,
+        borderColor: "gray",
+        borderRadius: 10,
+        padding: 20,
+    },
+    x: {
+        display: "flex",
+        justifyContent: "center"
+    },
+    xtxt: {
+        color: "red",
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    price: {
+        fontSize: 23,
+        fontWeight: "bold",
+    },
+    info: {
+        fontSize: 21,
+    },
+    total: {
+        textAlign: "center",
+        fontSize: 30,
+        fontWeight: "900"
+    }
+})
+
+export default App
--- calculadora-atualizada/app/ifome/index.jsx
+++ calculadora-atualizada/app/ifome/index.jsx
@@ -3,27 +3,28 @@ import {useState, useContext, useEffect} from "react";
 import { AppContext } from "../../scripts/AppContext";
 import {View, Text, Image, StyleSheet, Button, Pressable, SafeAreaView, FlatList} from 'react-native';
 import Header from "../../components/header";
+import {Link} from "expo-router";
 
 
 const Item = ({name, vendor, price, img, id}) => {
     const {cart, setCart} = useContext(AppContext);
 
-    console.log(cart)
     return(
-        <View>
+        <View style={styles.itemContainer}>
             <View>
                 <Image
                     source={{uri: img}}
+                    style={styles.img}
                 />
             </View>
             <View>
-                <Text>{name}</Text>
-                <Text>{vendor}</Text>
-                <Text>{price}</Text>
-                <Pressable onPress={() => {
-                    setCart([...cart, id]);
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.vendor}>{vendor}</Text>
+                <Text style={styles.price}>R$ {price}</Text>
+                <Pressable style={styles.buy} onPress={() => {
+                    setCart([...cart, {"id": id, "name": name, "vendor": vendor, "price": price}]);
                 }}>
-                    <Text>Comprar</Text>
+                    <Text style={styles.buyTxt}>Adicionar ao carrinho</Text>
                 </Pressable>
             </View>
         </View>
@@ -36,22 +37,102 @@ const App = () => {
 
     return(
         <SafeAreaView style={styles.container}>
-            <View>
+            <View style={styles.header}>
                 <Header link=".." title="iFome" />
-                <Text>Carrinho: {cart.length}</Text>
+                <View style={styles.cartArea}>
+                    <Image source={require('../../assets/cart.png')} style={styles.cartImg}/>
+                    <Text style={styles.txtCart}>{cart.length} itens</Text>
+                    {
+                        cart.length > 0 &&
+                        <Link href="./cart" style={styles.link}>
+                            <Text>Finalizar compra</Text>
+                        </Link>
+                    }
+                </View>
             </View>
-            <View>
-                <FlatList data={foods} renderItem={({item}) => (
-                    <Item name={item.name} vendor={item.vendor} price={item.price} img={item.img} id={item.id} />
+            <FlatList data={foods} renderItem={({item}) => (
+                <Item name={item.name} vendor={item.vendor} price={item.price} img={item.img} id={item.id} />
                 )} keyExtractor={item => item.id.toString()}
-                />
-            </View>
+            />
+
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    img: {
+        width: 150,
+        height: 150,
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    cartArea: {
+        textAlign: "right",
+        padding: 20,
+        display: "flex",
+        flexDirection: "row",
 
+    },
+    txtCart: {
+        marginRight: 20,
+        display: "flex",
+        alignItems: "center",
+    },
+    link: {
+        backgroundColor: '#007bff',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        color: 'white'
+    },
+    container: {
+        flex: 1,
+    },
+    header: {
+        marginBottom: 20,
+    },
+    itemContainer: {
+        display: "flex",
+        flexDirection: "row",
+        marginBottom: 40,
+        borderWidth: 3,
+        borderColor: "gray",
+        borderRadius: 10,
+        padding: 20,
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    vendor: {
+        fontSize: 14,
+        color: '#666',
+        marginBottom: 5,
+    },
+    price: {
+        fontSize: 16,
+        color: '#000',
+        marginBottom: 10,
+        fontWeight: 'bold',
+    },
+    buy: {
+        backgroundColor: '#007bff',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        alignItems: 'center',
+        width: 170
+    },
+    buyTxt: {
+        color: 'white'
+    },
+    cartImg: {
+        width: 30,
+        height: 30,
+        marginRight: 20
+    }
 })
 
 export default App;
